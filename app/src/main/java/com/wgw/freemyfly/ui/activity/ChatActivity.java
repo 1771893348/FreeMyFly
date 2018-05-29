f20a5f9bab98159f2c20d23b522df9d58655c5cf
wgw <1771893348@qq.com>
2018-05-29 18:31:27 +0800

    socket开发

--- app/src/main/java/com/wgw/freemyfly/ui/activity/ChatActivity.java
+++ app/src/main/java/com/wgw/freemyfly/ui/activity/ChatActivity.java
@@ -8,10 +8,14 @@
 
 package com.wgw.freemyfly.ui.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -58,6 +62,10 @@ public class ChatActivity extends MVPBaseActivity<IChatView,ChatPresenter> imple
                 case 0:
                     tv_receive_message.setText(stringBuffer.toString());
                     break;
+                case 1:
+                    tv_receive_message.setText(stringBuffer.toString());
+                    et_edit.setText("");
+                    break;
             }
         }
 
@@ -68,12 +76,14 @@ public class ChatActivity extends MVPBaseActivity<IChatView,ChatPresenter> imple
         super.onCreate(savedInstanceState);
         stringBuffer = new StringBuffer();
         initListener();
+        verifyStoragePermissions(this);
     }
 
     private void initListener() {
         mChatPresenter.setOnclickListener(btn_connect);
         mChatPresenter.setOnclickListener(btn_disconnect);
         mChatPresenter.setOnclickListener(btn_Receive);
+        mChatPresenter.setOnclickListener(btn_send);
     }
 
     @Override
@@ -118,7 +128,7 @@ public class ChatActivity extends MVPBaseActivity<IChatView,ChatPresenter> imple
 
     @Override
     public EditText getSendMessage() {
-        return null;
+        return et_edit;
     }
 
     @Override
@@ -127,5 +137,25 @@ public class ChatActivity extends MVPBaseActivity<IChatView,ChatPresenter> imple
         mHandler.sendEmptyMessage(0);
     }
 
+    @Override
+    public void sendMsgOver(String msg) {
+        stringBuffer.append(msg);
+        mHandler.sendEmptyMessage(1);
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+    }
 
 }
