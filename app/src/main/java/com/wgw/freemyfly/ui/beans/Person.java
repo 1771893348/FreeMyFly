e9db45b6092bdd0a9f32ebafecc2a5e230312457
wgw <1771893348@qq.com>
2018-06-07 09:43:48 +0800

    resyclerView的adapter与viewholder封装

---
+++ app/src/main/java/com/wgw/freemyfly/ui/beans/Person.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.wgw.freemyfly.ui.beans;
+
+/**
+ * Created by wgw on 2018/6/5.
+ */
+
+public class Person {
+    private String userName;
+    private int userAge;
+    private String iconUrl;
+    private int sex;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
